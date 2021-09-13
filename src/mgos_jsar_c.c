5a202b07cfa1e79bc279d0c5076e7ba4bd9fb397
levdm <levchenko.dmiitry@gmail.com>
2021-09-13 16:14:38 +0300

    fix init

--- src/mgos_jsar_c.c
+++ src/mgos_jsar_c.c
@@ -5,15 +5,6 @@
 
 bool  mgos_mgos_jsar_init(void) 
 {
-
-/*
-  if( mgos_jsar_begin() == 0) { 
-    LOG(LL_INFO, ("JSAR_INIT Complete"));
-  } else {
-    LOG(LL_INFO, ("JSAR_INIT FALSE"));
-  }
-*/
-
-  return MGOS_APP_INIT_SUCCESS;
+  return true;
 }
 
